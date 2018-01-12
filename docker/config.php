9430824770874924e5d470fd71c979ab7ed875aa
Gary Feltham <gary.feltham@citypay.com>
2018-01-12 14:38:26 +0000

    addition of docker

---
+++ docker/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', '/opencart/catalog/');
+define('DIR_SYSTEM', '/opencart/system/');
+define('DIR_IMAGE', '/opencart/image/');
+define('DIR_LANGUAGE', '/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/opencart/system/config/');
+define('DIR_CACHE', '/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opencart/system/storage/download/');
+define('DIR_LOGS', '/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'ocdb');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'P4ssw0rd!');
+define('DB_DATABASE', 'ocdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ docker/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+service php5-fpm start
+nginx -g 'daemon off;'
